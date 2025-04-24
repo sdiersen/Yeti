53144eab0ba396cd79fe02a5bdcd1c0af6590648
Steve Diersen <srdiersen@gmail.com>
2025-04-24 09:05:57 -0500

    Modified RoleController to use the new persistence model. Only method in RoleController is GetAllRoles.

--- backend/Controllers/Identity/RoleController.cs
+++ backend/Controllers/Identity/RoleController.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
 using Persistence.Models.Identity;
+using Persistence.Services.Identity;
 
 namespace backend.Controllers.Identity
 {
     public class RoleController : AbstractBaseController<Role>
     {
-        public RoleController(ILogger<RoleController> logger)
+        private readonly IRoleServices _roleServices;
+        public RoleController(ILogger<RoleController> logger, IRoleServices roleServices)
             : base(logger)
         {
+            _roleServices = roleServices;
+        }
+
+        [HttpGet("GetAllRoles")]
+        public async Task<ActionResult> GetAllRoles()
+        {
+            var returnValue = await _roleServices.GetAllRolesAsync();
+            if (!returnValue.Success)
+            {
+                return BadRequest(returnValue);
+            }
+            if (returnValue.Data == null || returnValue.Data.Count == 0)
+            {
+                return NotFound("No roles found.");
+            }
+            return Ok(returnValue);
         }
     }
 }
