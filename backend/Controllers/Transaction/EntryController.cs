554035e3b4f0ca18e2c3942282260892cd87abed
Steve Diersen <srdiersen@gmail.com>
2025-05-05 11:13:16 -0500

    Added EntryController with CreateEntry method

--- backend/Controllers/Transaction/EntryController.cs
+++ backend/Controllers/Transaction/EntryController.cs
@@ -1,15 +1,29 @@
 using Persistence.Models.Transaction;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
+using Persistence.DTOs.Transaction;
+using Persistence.Services.Transaction;
 
 namespace backend.Controllers.Transaction
 {
     public class EntryController : AbstractBaseController<Entry>
     {
-
-        public EntryController(ILogger<EntryController> logger)
+        private readonly IEntryServices _entryServices;
+        public EntryController(ILogger<EntryController> logger, IEntryServices entryServices)
         : base(logger)
         {
+            _entryServices = entryServices;
+        }
+
+        [HttpPost("CreateEntry")]
+        public async Task<ActionResult> CreateEntry([FromBody] EntryDTO entryDTO)
+        {
+            var results = await _entryServices.CreateEntryAsync(entryDTO);
+            if (!results.Success)
+            {
+                return BadRequest(results);
+            }
+            return Ok(results);
         }
     }
 }
